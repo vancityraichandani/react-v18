ad0a9f9c52c52335a7803454dea8e9bd27abf968
vancityraichandani <someshraichandani1@gmail.com>
2023-03-13 01:39:43 +0530

    ref2

--- src/App.js
+++ src/App.js
@@ -2,41 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import Comp1 from './components/Comp1';
 import { useState, useEffect, useRef } from 'react';
-console.log('outside')
+
 function App() {
-  const [st, setSt] = useState({ k1: 'Key1', k2: 'Key2' })
+  // const [st, setSt] = useState({ k1: 'Key1', k2: 'Key2' })
   const [a, sa] = useState(0)
-  const r = useRef(0)
+  const c = useRef(1);
 
-  useEffect(() => {
-    console.log('effect');
-  })
-  
-  console.log('render comp' + a);
-  
-  const handleClick = () => {
-    const newObj = { ...st };
-    newObj.k1 = 'hi'
-    setSt(newObj);
-    sa(pa => pa + 1)
-    sa(pa => pa + 1)
-    console.log('refVal', r.current);
-    anotherfn(a);
-    console.log('click' + a);
+  console.log('render');
+
+  function handleClick() {
+    console.log('ref change');
+    c.current += 1;
   }
-
-  const anotherfn = () => {
-    r.current += 5
-    console.log('another' + a);
+  function handlesc() {
+    console.log('state change');
+    sa(a => a + 1)
   }
 
   return (
     <>
       <div className="App">
-        <p onClick={handleClick} className="para">{st.k1}</p>
-        <p onClick={handleClick} className="para">{a}</p>
-        <p onClick={handleClick} className="para">{st.k2}</p>
-        <p onClick={handleClick} className="para">{r.current}</p>
+        <p onClick={handleClick} className="para">{c.current}</p>
+        <p onClick={handlesc} className="para">{a}</p>
         <Comp1 />
       </div>
     </>
