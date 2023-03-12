81702ea12a52922646cfc70a5c91110dae53f6ff
vancityraichandani <someshraichandani1@gmail.com>
2023-03-13 02:25:50 +0530

    usetransition

--- src/App.js
+++ src/App.js
@@ -5,32 +5,27 @@ import { useState, useEffect, useRef } from 'react';
 
 function App() {
   // const [st, setSt] = useState({ k1: 'Key1', k2: 'Key2' })
-  const [a, sa] = useState('')
-  const [ct, sct] = useState(0)
+  const [a, sa] = useState(0)
+  const [b, sb] = useState(5)
   const c = useRef(1);
 
   console.log('render');
 
-  useEffect(()=>{
-    sct(ct => ct + 1);
-  })
-
-  function handleClick() {
-    // console.log('ref change');
-    // c.current += 1;
+  function handleA() {
   }
-  function handlesc() {
-    console.log('state change');
+  function handleB() {
     sa(a => a + 1)
+    console.log('a changed');
+    for(let i = 0; i < 3000000000; i++) { }
+    sb(b => b + 1)
+    console.log('b changed');
   }
 
   return (
     <>
       <div className="App">
-        <input value={a} type="text"/>
-        <p onClick={handleClick} className="para">{ct}</p>
-        {/* <p onClick={handleClick} className="para">{c.current}</p> */}
-        <p onClick={handlesc} className="para">{a}</p>
+        <p onClick={handleA} className="para">{a}</p>
+        <p onClick={handleB} className="para">{b}</p>
         <Comp1 />
       </div>
     </>
